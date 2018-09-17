3edd82ef3e7495c323ab8fb424befd574789b81a
Jess Unrein <junrein@Jesss-MacBook-Pro.local>
2018-09-17 09:12:19 -0500

    two threads running concurrently.

--- tasks.py
+++ tasks.py
@@ -4,11 +4,20 @@ import threading
 
 def countdown(count):
     while count >= 0:
-        print("Count down {}".format(count))
+        print("{} Count down {}".format(threading.current_thread(), count))
         count -= 1
+        time.sleep(1)
+
+def countup(count):
+    while count <= 10:
+        print("{} Count up {}".format(threading.current_thread(), count))
+        count += 1
         time.sleep(1)
 
 t1 = threading.Thread(name='countdown', args=(10,), target=countdown)
 t1.start()
+
+t2 = threading.Thread(name='countdown', args=(0,), target=countup)
+t2.start()
 
 print("complete")
